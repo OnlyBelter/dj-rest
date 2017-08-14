d42dea096104394bb0797dca67edb71d90fab8d0
OnlyBelter <xiongxin20008@126.com>
2017-08-14 18:40:15 +0800

    support CORS

--- issBackend/issBackend/settings.py
+++ issBackend/issBackend/settings.py
@@ -55,7 +55,6 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # <-- for CORS
-    'django.middleware.common.CommonMiddleware',  # <-- for CORS
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True  # <-- for CORS
 
 ROOT_URLCONF = 'issBackend.urls'
 
