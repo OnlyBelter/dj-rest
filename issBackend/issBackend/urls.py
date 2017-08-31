48d324affc54f39654b38994b277988417f0d847
OnlyBelter <xiongxin20008@126.com>
2017-08-31 17:08:08 +0800

    add token verify and refresh

--- issBackend/issBackend/urls.py
+++ issBackend/issBackend/urls.py
@@ -20,6 +20,8 @@ from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
 
 
 router = DefaultRouter()
@@ -33,5 +35,7 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', obtain_jwt_token)
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
